d5552bd2c2c3994ffb1f66618bad56fdbf24d136
yeshwanth <yeshwanthds2002@gmail.com>
2024-11-12 01:18:14 +0530

    project updated

--- backend/index.js
+++ backend/index.js
@@ -43,11 +43,11 @@ app.listen(PORT, () => {
   console.log("Server is running on port:", PORT);
 });
 // Schedule a cron job to run every day at 10:30 PM
-cron.schedule('51 23 * * *', async () => {  // Runs every day at 10:30 PM
+cron.schedule('30 1 * * *', async () => {  // Runs every day at 10:30 PM
   console.log("Cron job started: Fetching upcoming estimated orders...");
   try {
-    const apiUrl = process.env.API_BASE_URL || 'http://localhost:5000/api/watch'; // Default to local API for dev
-    const response = await axios.get(`${apiUrl}/getUpcomingEstimatedOrders`);
+    const apiUrl = process.env.API_BASE_URL || 'http://localhost:5000'; // Default to local API for dev
+    const response = await axios.get(`${apiUrl}/api/watch/getUpcomingEstimatedOrders`);
     console.log(apiUrl);
     console.log('Scheduled task response:', response.data); // Log the response data
   } catch (error) {
@@ -56,11 +56,11 @@ cron.schedule('51 23 * * *', async () => {  // Runs every day at 10:30 PM
 });
 
 // Schedule a cron job to run every 1st day of the month at 10:30 PM
-cron.schedule('55 23 * * *', async () => {  // Runs every 1st day of the month at 10:30 PM
+cron.schedule('30 1 * * *', async () => {  // Runs every 1st day of the month at 10:30 PM
   console.log("Cron job started: Fetching delivered watches...");
   try {
-    const apiUrl = process.env.API_BASE_URL || 'http://localhost:5000/api/watch'; // Default to local API for dev
-    const response = await axios.get(`${apiUrl}/deliveredWatches`);
+    const apiUrl = process.env.API_BASE_URL || 'http://localhost:5000'; // Default to local API for dev
+    const response = await axios.get(`${apiUrl}/api/watch/deliveredWatches`);
     console.log('Scheduled task response (deliveredWatches):', response.data); // Log the response data
   } catch (error) {
     console.error('Error in scheduled task (deliveredWatches):', error.message); // Handle errors in the scheduled task
